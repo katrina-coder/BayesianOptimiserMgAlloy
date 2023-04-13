5d97a8c54d49273967db9d6f80a6530ddbebcca7
atiyehp <42898707+atiyehp@users.noreply.github.com>
2023-04-13 12:47:00 +0330

    Update alloysHT.py

--- alloysHT.py
+++ alloysHT.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 
 
-data = pd.read_excel('M-916 - wt%.xlsx') #.drop(columns=['Name', 'Condition', 'Sum'])
+data = pd.read_excel('BayesianOptimiserMgAlloy/M-916 - wt%.xlsx') #.drop(columns=['Name', 'Condition', 'Sum'])
 data["Coded"].replace({1: "Extruded", 2: "ECAP", 3: "Cast_Slow", 4: "Cast_Fast", 5: "Cast_HT", 6:"Wrought"}, inplace=True)
 data.rename(columns= {"Coded": "Processing"} , inplace=True )
 data = pd.get_dummies(data, columns=['Processing'])
